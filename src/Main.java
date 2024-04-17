f20e45c0f2223ee42e1c647eb9de5cc2cddeaa64
Ethan Tate <50052151@serc.ac.uk>
2024-04-17 16:46:07 +0100

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<String>();
+        name.add("Alice");
+        name.add("Bob");
+        name.add("Charlie");
+        name.add("David");
+        name.add("Emma");
+        name.set(3, "Daniel");
+        //name.remove(3);
+        name.clear();
+
+        System.out.println(name);
+
+        Collections.sort(name);
+
+        //System.out.println("Sorted list: " + name);
+
+        //System.out.println(name);
+
+        //System.out.println(name);
+
+        //for (int i = 0; i < name.size(); i++)
+        {
+            //System.out.println(name.get(i));
+        }
+
+        //System.out.println("does names contain Alice?");
+        //System.out.println(name.contains("Alice"));
+
+
+
+
+
 
     }
 }
+
